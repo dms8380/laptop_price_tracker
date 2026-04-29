7aefafa89c2519eaa5623e4d9080909f559ab68f
dms8380 <dms8380@psu.edu>
2026-04-28 23:34:38 -0400

    2nd update for data_manager

--- scraper/data_manager.py
+++ scraper/data_manager.py
@@ -1,27 +1,12 @@
 """
 data_manager.py
----------------
 Computes summary statistics from a list of laptop dicts.
-
-Each laptop dict has keys:
+Each laptop dict has keys
     name (str), price (float), price_str (str), rating (float)
 """
 
-
 def get_statistics(laptops: list[dict]) -> dict:
-    """
-    Compute summary statistics for the scraped laptop dataset.
-
-    Returns a dict with:
-        total          - number of laptops
-        avg_price      - average price
-        min_price      - cheapest price
-        max_price      - most expensive price
-        avg_rating     - average star rating
-        highest_rated  - name of the top-rated laptop
-        lowest_priced  - name of the cheapest laptop
-        rated_count    - how many laptops have a rating > 0
-    """
+    #Compute summary statistics for the scraped laptop dataset (ex. total, average price, minimum price)
     if not laptops:
         return {}
 
